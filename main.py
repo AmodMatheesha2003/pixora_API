9467e6b4b7f9ed56b4d0b649adbac8e2adf1aed7
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-03-15 12:00:17 +0530

    Small Changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="pixora API",
+              description="Blockchain-based Photos/Digital Art publishing, buying & selling platform")
 
 @app.get("/")
 def read_root():
     return {"Pixora FastAPI"}
-
